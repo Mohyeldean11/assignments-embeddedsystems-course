3347ea81ede24325623c222102b57ec4269962fe
mohy <mohyeldean@gmail.com>
2021-12-22 16:40:22 +0200

    Adding cod outline and documentation for C1M1 Assessment

--- stats.c
+++ stats.c
@@ -9,13 +9,13 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file <STATS.c> 
+ * @brief <ASSIGNMENT 1 >
  *
  * <Add Extended Description Here>
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author <MOHIEE ELDEAN EHAB>
+ * @date <12/22/2022 >
  *
  */
 
@@ -27,7 +27,9 @@
 /* Size of the Data Set */
 #define SIZE (40)
 
-void main() {
+
+void main()
+{
 
   unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
                               114, 88,   45,  76, 123,  87,  25,  23,
@@ -36,8 +38,163 @@ void main() {
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
   /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
 
+  unsigned char *ptr; //CREATING A POINTER 
+  ptr=test;  //POINTING TO THE ARRAY 
+
+ int median=0;
+ int mean=0;
+ int maximum=0;
+ int minimum=0;
+
+  /* Statistics and Printing Functions Go Here */
+  
+  print_array(SIZE,ptr); //PRINTING THE ARRAY
+  printf("\n");
+
+  median=find_median(SIZE,ptr); //assigning the median to a variable
+  printf("the median is: %d \n",median); //printing the median of the array
+  printf("\n");
+  
+  mean=find_mean(SIZE,ptr); //assigning the mean to a variable
+  printf("the mean is: %d \n",mean); //printing the mean of the array
+  printf("\n");
+
+  maximum=find_maximum(SIZE,ptr); //assigning the maximum to a variable
+  printf("the maximum is: %d \n",maximum); //printing the maximum of the array
+  printf("\n");
+  
+  minimum=find_minimum(SIZE,ptr);
+  printf("the minimum is: %d \n",minimum); //printing the maximum of the array
+  printf("\n");
+ 
+  sort_array(SIZE,ptr); //sorting the array ascendingly
+  printf("\n");
+
+  print_statistics(SIZE,ptr);
 }
 
 /* Add other Implementation File Code Here */
+
+int print_array(int length,unsigned char *pointer)
+{
+  printf("the array : \n");
+  for(int i = 0;i< length; i++ )
+  {
+    printf("%d",pointer[i]);
+    printf("\t");
+  }
+  printf("\n");
+}
+
+int find_median(int length,unsigned char *pointer)
+{
+  int median=0;
+  int first_mean=0;
+  int second_mean=0;
+
+  if ( length%2 == 0)
+  {
+  first_mean=pointer[(length/2)] ;
+  second_mean=pointer[(length/2)+1] ;
+
+  median=(first_mean+second_mean)/2 ;
+  
+  }
+  else if (length%2 != 0)
+  {
+    median=pointer[length/2];
+  }
+  
+  return median;
+}
+
+int find_mean(int length,unsigned char *pointer)
+{
+  int sum=0;
+  int mean=0;
+  for(int i=0 ; i < length ; i++){
+
+    sum+=pointer[i];
+
+  }
+  mean = sum/length;
+  return mean;
+}
+
+int find_maximum(int length,unsigned char *pointer)
+{
+  int maximum=pointer[0];
+
+  for (int i = 0; i < length; i++)
+  {
+    if(maximum<pointer[i])
+    {
+      maximum=pointer[i];
+    }
+  }
+  return maximum;
+}
+
+int find_minimum(int length,unsigned char *pointer)
+{
+  int minimum=pointer[0];
+
+  for (int i = 0; i < length; i++)
+  {
+    if(minimum>pointer[i])
+    {
+      minimum=pointer[i];
+    }
+  }
+  return minimum;
+}
+
+int sort_array(int length, unsigned char *pointer)
+{
+
+  int current=0;
+  for (int i = 0; i < length; i++)
+  {
+    for (int j = 0; j <= i; j++)
+    {
+      if (pointer[i] > pointer[j])
+      {
+        current=pointer[i];
+        pointer[i]=pointer[j];
+        pointer[j]=current;
+      }
+    }
+  }
+  printf("the array after sorting\n");
+  print_array(length,pointer);
+  
+}
+
+int print_statistics(int length, unsigned char *pointer)
+{
+  int median=0;
+  int mean=0;
+  int maximum=0;
+  int minimum=0;
+
+  printf("ALL THE INFO FOR THE ARRAY: \n");
+  printf("\n");
+  
+  median=find_median(length,pointer); //assigning the median to a variable
+  printf("the median is: %d \n",median); //printing the median of the array
+  printf("\n");
+  
+  mean=find_mean(length,pointer); //assigning the mean to a variable
+  printf("the mean is: %d \n",mean); //printing the mean of the array
+  printf("\n");
+
+  maximum=find_maximum(length,pointer); //assigning the maximum to a variable
+  printf("the maximum is: %d \n",maximum); //printing the maximum of the array
+  printf("\n");
+  
+  minimum=find_minimum(length,pointer);
+  printf("the minimum is: %d \n",minimum); //printing the maximum of the array
+  printf("\n");
+
+}
--- stats.h
+++ stats.h
@@ -9,32 +9,111 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file <STATS.c> 
+ * @brief <ASSIGNMENT 1 >
  *
  * <Add Extended Description Here>
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author <MOHIEE ELDEAN EHAB>
+ * @date <12/22/2022 >
  *
  */
+
 #ifndef __STATS_H__
 #define __STATS_H__
 
 /* Add Your Declarations and Function Comments here */ 
-
+int print_array(int length,unsigned char *pointer);
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief <THIS FUNCTION IS TO PRINT THE ARRAY>
  *
  * <Add Extended Description Here>
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @param <size> <THE length OF THE ARRAY>
+ * @param <pointer > <THE POINTER TO THE ARRAY THAT IS GOING TO BE PRINTED>
  * @param <Add InputName> <add description here>
  * @param <Add InputName> <add description here>
  *
- * @return <Add Return Informaiton here>
+ * @return <NULL>
  */
 
+int sort_array(int length,unsigned char *pointer);
+/**
+ * @brief <THIS FUNCTION IS TO SORTING THE ARRAY>
+ *
+ * <Add Extended Description Here>
+ *
+ * @param <length> <THE length OF THE ARRAY>
+ * @param <pointer > <THE POINTER TO THE ARRAY THAT IS GOING TO BE PRINTED>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ *
+ * @return <NULL>
+ */
 
+int find_median(int length,unsigned char *pointer);
+/**
+ * @brief <THIS FUNCTION IS TO FINDING THE MEDIAN IN THE ARRAY>
+ *
+ * <Add Extended Description Here>
+ *
+ * @param <length> <THE length OF THE ARRAY>
+ * @param <pointer > <THE POINTER TO THE ARRAY THAT IS GOING TO BE PRINTED>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ *
+ * @return <median>
+ */
+int find_mean(int length,unsigned char *pointer);
+/**
+ * @brief <THIS FUNCTION IS TO FIND THE MEAN IN THE ARRAY>
+ *
+ * <Add Extended Description Here>
+ *
+ * @param <size> <THE length OF THE ARRAY>
+ * @param <pointer > <THE POINTER TO THE ARRAY THAT IS GOING TO BE PRINTED>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ *
+ * @return <MEAN>
+ */
+int find_maximum(int length,unsigned char *pointer);
+/**
+ * @brief <THIS FUNCTION IS TO FIND THE MAXIMUM IN THE ARRAY>
+ *
+ * <Add Extended Description Here>
+ *
+ * @param <size> <THE length OF THE ARRAY>
+ * @param <pointer > <THE POINTER TO THE ARRAY THAT IS GOING TO BE PRINTED>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ *
+ * @return <MAXIMUM>
+ */
+int find_minimum(int length,unsigned char *pointer);
+/**
+ * @brief <THIS FUNCTION IS TO FIND THE MINIMUM IN THE ARRAY>
+ *
+ * <Add Extended Description Here>
+ *
+ * @param <size> <THE length OF THE ARRAY>
+ * @param <pointer > <THE POINTER TO THE ARRAY THAT IS GOING TO BE PRINTED>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ *
+ * @return <MINIMUM>
+ */
+int print_statistics(int length, unsigned char *pointer);
+/**
+ * @brief <THIS FUNCTION IS TO FIND ALL INFORMATION  ABOUT THE ARRAY>
+ *
+ * <FINDING THE MAXIMUM,MINIMUM,MEAN,MEDIAN OF THE ARRAY>
+ *
+ * @param <size> <THE length OF THE ARRAY>
+ * @param <pointer > <THE POINTER TO THE ARRAY THAT IS GOING TO BE PRINTED>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ *
+ * @return <NULL>
+ */
 #endif /* __STATS_H__ */
